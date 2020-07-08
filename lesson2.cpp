948f440b7ddfd7417203a2f10ddfc9baa709c10c
DEVRAJ22 <44719782+DEVRAJ22@users.noreply.github.com>
2020-07-08 15:12:46 +0530

    Add files via upload

---
+++ lesson2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+/*int glo = 6;
+void sum()
+{
+    int a;
+    cout << glo;
+}*/
+
+int main()
+{
+    /*int glo = 9;
+    glo = 78;
+    // int a = 14;
+    // int b = 15;
+    int a = 14, b = 15;
+    float pi = 3.14;
+    char c = 'd';
+    bool is_true = false;
+    sum();
+    cout << glo << is_true;
+    // cout<<"This is tutorial 4.\nHere the value of a is "<<a<<".\nThe value of b is "<< b;
+    // cout<<"\nThe value of pi is: "<<pi;
+    // cout<<"\nThe value of c is: "<<c;*/
+
+    int num1, num2;
+    cout<<"what is your first number?\n";
+    cin>>num1;
+
+    cout << "what is your second number?\n";
+    cin >> num2;
+    
+    cout<<"the sum of your number is= "<<num1+num2;
+
+    return 0;
+}
